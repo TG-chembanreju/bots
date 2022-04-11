974d36085eb14671859b67c7ae000c7574b3f704
TG-PushpaReju <97326699+TG-chembanreju@users.noreply.github.com>
2022-04-11 17:34:53 +0530

    Update Pushpareju.py

--- Pushpareju.py
+++ Pushpareju.py
@@ -1,14 +1,14 @@
-
 from pyrogram import Client
 
 PyrogramBot = Client(
     "PyrogramBot",
     api_hash="89b2d2c0120a5ae9e41b9891fdcc3f8f",
     api_id="9024035",
-    bot_token="5262055140:AAFFRMcO-IQvBFzagPaMlqadQ8xLENfhtCQ",
+    bot_token="5122697842:AAELdBHi56NQqew3v2x8Kv9QL9hs14YJdto",
     plugins=dict(root="PyrogramBot")
   )
 
 
 PyrogramBot.run()
+    
     
